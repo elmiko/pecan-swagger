9f704b875767cf7fcec18d659489a86b4f65819c
Michael McCune <msm@redhat.com>
2015-01-15 10:51:37 -0500

    adding a little sugar to make the json sweeter

--- pecan_swagger/utils.py
+++ pecan_swagger/utils.py
@@ -1,12 +1,14 @@
+import collections
 import json
 
 from pecan_swagger import g as g
 
 
 def swagger_build(title, version):
-    swag = dict(swagger='2.0',
-                info=dict(title=title, version=version),
-                paths={})
+    swag = collections.OrderedDict()
+    swag['swagger']='2.0'
+    swag['info']=dict(title=title, version=version)
+    swag['paths']={}
     for p in g.get_paths():
         swag['paths'][p[0]] = {k: {} for k in p[1]}
     return json.dumps(swag, indent=2)
