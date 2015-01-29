b0e690928cd72117dbcb87ab48f211871791291e
Michael McCune <msm@redhat.com>
2015-01-28 23:51:46 -0500

    refactoring decorator names for clarity

--- pecan_swagger/decorators.py
+++ pecan_swagger/decorators.py
@@ -1,18 +1,18 @@
 import pecan_swagger.g as g
 
 def path(endpoint, name, parent=None):
-    def pathwrap(c):
+    def decorator(c):
         if hasattr(c, '__swag'):
             raise Exception('{} already has swag'.format(c.__name__))
         c.__swag = dict(endpoint=endpoint, name=name, parent=parent)
         g.add_path(c)
         return c
-    return pathwrap
+    return decorator
 
 def method(method):
-    def methodwrap(m):
+    def decorator(m):
         if hasattr(m, '__swag'):
             raise Exception('{} already has swag'.format(m.__name__))
         m.__swag = dict(method=method)
         return m
-    return methodwrap
+    return decorator
