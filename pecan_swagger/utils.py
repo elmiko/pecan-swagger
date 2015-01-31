2898afb289d2637cf6ef005238a64c4195a2b91d
Michael McCune <msm@redhat.com>
2015-01-31 15:14:57 -0500

    removing empty paths from swagger builder
    this change makes utils.swagger_builder not add paths which have no
    methods.

--- pecan_swagger/utils.py
+++ pecan_swagger/utils.py
@@ -10,5 +10,6 @@ def swagger_build(title, version):
     swag['info']=dict(title=title, version=version)
     swag['paths']={}
     for p in g.get_paths():
-        swag['paths'][p[0]] = {k: {} for k in p[1]}
+        if len(p[1]) > 0:
+            swag['paths'][p[0]] = {k: {} for k in p[1]}
     return json.dumps(swag, indent=2)
