c4905d1c9f8bf3604335dae41c073779432cd1f7
Michael McCune <msm@redhat.com>
2015-01-31 15:20:57 -0500

    adding a bunch of comments

--- pecan_swagger/g.py
+++ pecan_swagger/g.py
@@ -2,12 +2,14 @@ _hierarchy = {}
 
 
 def add_path(c):
+    '''adds a named controller to the hierarchy.'''
     if _hierarchy.get(c.__swag['name']):
         raise Exception('name {} already exists in hierarchy'.format(c.__swag['name']))
     _hierarchy[c.__swag['name']] = c
 
 
 def build_path(swaginfo):
+    '''return the route path for a swag metadata item.'''
     if swaginfo.get('parent') is not None:
         return path_join(build_path(get_swag(swaginfo.get('parent'))),
                          swaginfo.get('endpoint'))
@@ -15,6 +17,14 @@ def build_path(swaginfo):
 
 
 def get_paths():
+    '''return all the registered paths
+
+    loops through the hierarchy and retuns a list of tuples containing the
+    paths and their methods.
+
+    :returns: [(path, methods), ...]
+
+    '''
     pathlist = []
     for name in _hierarchy:
         fullpath = build_path(get_swag(name))
@@ -25,10 +35,12 @@ def get_paths():
 
 
 def get_swag(name):
+    '''return the swag metadata from an named controller.'''
     return _hierarchy.get(name).__swag
 
 
 def methods_get(name):
+    '''get all the methods for a named controller.'''
     c = _hierarchy[name]
     mlist = []
     for i in c.__dict__:
