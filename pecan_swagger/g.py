972d61f20636e5fb3a28f3e2c9c852971d8af0eb
shu-mutou <shu-mutou@rf.jp.nec.com>
2016-07-06 23:08:27 +0900

    Add "parameters" and "responses" from WSME source code. (#6)
    * Add WSME params and response into each HTTP methods
    
    * Comment out temporary file output.
    
    * implovement against result by "swagger-tools validate"

--- pecan_swagger/g.py
+++ pecan_swagger/g.py
@@ -3,6 +3,7 @@ import inspect
 
 from pecan import util as p_u
 import six
+import string
 
 
 """
@@ -18,9 +19,9 @@ hierarchy of controllers, routes, and methods.
 
 _hierarchy = {}
 
-_http_methods = {'GET': '', 'POST': '', 'PUT': '',
-                 'PATCH': '<specifier>', 'DELETE': '<specifier>',
-                 'HEAD': '', 'TRACE': ''}
+_http_methods = {'get': '', 'post': '', 'put': '',
+                 'patch': '<specifier>', 'delete': '<specifier>',
+                 'head': '', 'trace': ''}
 
 def add_path(c):
     """adds a named controller to the hierarchy."""
@@ -38,7 +39,7 @@ def build_path(swaginfo):
     return swaginfo.get('endpoint')
 
 
-def get_controller_paths(controllers):
+def get_controller_paths(controllers, wsme_defs):
     """
     get a list of paths with methods
 
@@ -49,7 +50,7 @@ def get_controller_paths(controllers):
         generic_handlers = lc.get(name).get('generic_handlers', {})
         for method, func in generic_handlers.items():
             if method == 'DEFAULT':
-                methods.append('GET')
+                methods.append('get')
             else:
                 methods.append(method)
             # TODO drill down through decorators to get true function name
@@ -61,6 +62,7 @@ def get_controller_paths(controllers):
         paths.append(tpl)
         del lc[name]
 
+
     lc = copy.deepcopy(controllers)
     paths = []
     # TODO incorporate method decorator, removing functions marked
@@ -71,23 +73,30 @@ def get_controller_paths(controllers):
     for method, path in _http_methods.items():
         func = method.lower()
         if lc.get(func):
-            append_methods_for_specific(func, (path, [method]))
+            spec = wsme_defs.get(func) # add wsme definitions
+            append_methods_for_specific(func, (path, (method.lower(), spec)))
 
     if lc.get('get_all'):
-        append_methods_for_specific('get_all', ('', ['GET']))
+        # add wsme definitions
+        spec = wsme_defs.get('get_all') # add wsme definitions
+        append_methods_for_specific('get_all', ('', ('get', spec)))
     if lc.get('get_one'):
-        append_methods_for_specific('get_one', ('<specifier>', ['GET']))
+        # add wsme definitions
+        spec = wsme_defs.get('get_one') # add wsme definitions
+        append_methods_for_specific('get_one', ('<specifier>', ('get', spec)))
 
     if lc.get('_default'):
-        append_methods_for_specific('_default', ('<default>', ['*']))
+        append_methods_for_specific('_default', ('<default>', ('*', {})))
     if lc.get('_route'):
-        append_methods_for_specific('_route', ('<route>', ['*']))
+        append_methods_for_specific('_route', ('<route>', ('*', {})))
     if lc.get('_lookup'):
         del lc['_lookup']
 
     if lc.get('_custom_actions'):
         for ca, ca_method in lc['_custom_actions'].items():
-            paths.append((ca, ca_method))
+            spec = wsme_defs.get(ca) # add wsme definitions
+            for m in ca_method:
+                paths.append((ca, (m.lower(), spec)))
         del lc['_custom_actions']
 
     generic_controllers = [c for c in lc if lc[c].get('generic')]
@@ -95,8 +104,223 @@ def get_controller_paths(controllers):
         paths.append((controller, get_methods_for_generic(controller)))
     for controller in lc:
         if controller not in [path[0] for path in paths]:
-            paths.append((controller, ['GET']))
+            paths.append((controller, ('get', {})))
     return paths
+
+def get_wsme_defs(name):
+
+    def datatype_to_type_and_format(datatype):
+        tf = {}
+        if datatype == 'uuid' or datatype == 'ipv4address' \
+             or datatype == 'ipv6address':
+            tf['type'] = 'string'
+            tf['format'] = datatype
+        elif datatype == 'datetime':
+            tf['type'] = 'string'
+            tf['format'] = 'date-time'
+        elif datatype == 'binary':
+            # base64 encoded characters
+            tf['type'] = 'string'
+            tf['format'] = 'byte'
+        elif datatype == 'array' or datatype == 'boolean' \
+             or datatype == 'integer' or datatype == 'string':
+            # no format
+            tf['type'] = datatype
+        elif 'unicode':
+            # primitive, no format
+            tf['type'] = 'string'
+        elif datatype == 'int':
+            # primitive, no format
+            tf['type'] = 'integer'
+        elif datatype == 'unset':
+            tf['type'] = None
+        elif datatype == 'dict':
+            tf['type'] = 'object'
+        elif datatype == 'enum':
+            tf['type'] = 'enum'
+        else:
+            tf['type'] = datatype
+        return tf
+
+    def get_type_str(obj, src_dict = None):
+        str = ''
+        if hasattr(obj, '__name__'):
+            str = obj.__name__
+        else:
+            str = obj.__class__.__name__
+        if str.endswith('Type'):
+            str = str[:-4]
+        str = string.lower(str)
+
+        if isinstance(src_dict, dict):
+            # if dict is in args, set 'type' and 'format' into the dict and return
+            tf = datatype_to_type_and_format(str)
+            src_dict.update({'type': tf['type']})
+            if 'format' in tf:
+                src_dict.update({'format': tf['format']})
+
+            # get datatype options. ex.) min_length, minimum, ..
+            for k, v in inspect.getmembers(obj):
+                if (k == 'minimum' or k == 'maxmum' \
+                    or k == 'min_length' or k == 'max_length') \
+                    and v is not None:
+                    src_dict[to_swag_key(k)] = v
+                elif k == 'pattern' and v is not None:
+                    src_dict[to_swag_key(k)] = v.pattern
+
+            return str
+        else:
+            # return 'type' only
+            return str
+
+    def to_swag_key(key):
+        keys = {
+            'doc': 'description',
+            'arguments': 'parameters',
+            'return_type': 'schema',
+            'datatype': 'type',
+            'mandatory': 'required',
+            'sample': 'examples',
+            'readonly': 'readOnly',
+            'min_length': 'minLength',
+            'max_length': 'maxLength',
+        }
+        if key in keys:
+            return keys[key]
+        else:
+            return key
+
+    def get_wm_item_prop(item, isparams=False):
+        # Add prop into 'properties' and 'required' in 'Items Object'
+        # 'Items Object' can be part of 'Schema Object' or 'Items Object',
+        # and can use recursively
+        prop_dict = {}
+        #prop_dict['obj'] = inspect.getmembers(item)
+        for a, i in inspect.getmembers(item):
+            if a == 'datatype':
+                get_type_str(i, prop_dict)
+                datatype = get_type_str(i, prop_dict)
+                if datatype == 'array':
+                    # if array, do recursively
+                    prop_dict['items'] = inspect_wm_schema(i.item_type)
+            elif a == 'default':
+                if get_type_str(i) == 'unset':
+                    #prop_dict[to_swag_key(a)] = None
+                    pass
+                else:
+                    prop_dict[to_swag_key(a)] = i
+            elif a == 'name' and isparams:
+                prop_dict[to_swag_key(a)] = i
+            elif a == 'mandatory' and i is not None and isparams:
+                prop_dict[to_swag_key(a)] = i
+            elif (a == 'readonly' or a == 'doc') \
+                 and i is not None:
+                prop_dict[to_swag_key(a)] = i
+
+        if isparams and prop_dict['type'] in ['object', 'array']:
+            prop_dict['schema'] = {'items': prop_dict['items'],
+                                   'type': prop_dict['type']}
+            del prop_dict['type']
+            del prop_dict['items']
+            del prop_dict['default']
+        return prop_dict
+
+    def get_wsattr_and_wsproperty(obj):
+        ws_dict = {}
+        for key, value in inspect.getmembers(obj):
+            if key[0] != '_' \
+               and (value.__class__.__name__ == 'wsattr' \
+                    or value.__class__.__name__ == 'wsproperty'):
+                ws_dict[key] = value
+        return ws_dict
+
+    def inspect_wm_schema(schema_obj, isparams=False):
+        schema_dict = {}
+        #schema_dict['obj'] = get_wsattr_and_wsproperty(schema_obj)
+        ws_len = len(get_wsattr_and_wsproperty(schema_obj))
+        for key, obj in inspect.getmembers(schema_obj):
+            if key[0] != '_' \
+               and (obj.__class__.__name__ == 'wsattr' \
+                    or obj.__class__.__name__ == 'wsproperty'):
+
+                if ws_len == 1:
+                    # single schema
+                    if get_type_str(obj.datatype) != 'array':
+                        # not array
+                        schema_dict[to_swag_key(key)] = get_wm_item_prop(obj, isparams)
+                    else:
+                        # single array
+                        schema_dict[to_swag_key(key)] = {'type': 'array'}
+                        schema_dict[to_swag_key(key)]['items'] = get_wm_item_prop(obj, isparams)
+                        # FIX: this can't get contents
+                else:
+                    # multi property schema or array
+                    schema_dict.update({'type': 'object'})
+                    # properties
+                    if 'items' not in schema_dict:
+                        schema_dict['items'] = {'properties': {}}
+                    schema_dict['items']['properties'].update({to_swag_key(key): get_wm_item_prop(obj, isparams)})
+                    # required as array of string
+                    # TODO: required as array of string
+                    #schema_dict['required'] = []
+
+            #if key == 'sample':
+                # TODO: example from sample
+                #'examples': {'application/json': {}},
+                # sample function doesn't work without API service in OpenStack Magnum
+                #schema_dict[to_swag_key(item)] = prop()
+                #schema_dict[to_swag_key(key)] = 'needs sample as examples'
+
+        return schema_dict
+
+    def get_wm_def(o):
+        wsme = {'description': ''}
+        for w, d in inspect.getmembers(o):
+            if w == 'arguments':
+                wsme[to_swag_key(w)] = []
+                for arg in d:
+                    # set each 'Parameter Object', it can include 'Items Object' recursively
+                    item_dict = get_wm_item_prop(arg, True)
+                    # TODO: MUST be set one of 'body|query|path|header|formData'
+                    item_dict['in'] = 'query'
+                    if 'schema' in item_dict:
+                        item_dict['in'] = 'body'
+                    wsme[to_swag_key(w)].append(item_dict)
+            elif w == 'doc' and d:
+                wsme[to_swag_key(w)] = d
+            elif w == 'return_type':
+                wsme['responses'] = {'status': {'description': ''}}
+                if d:
+                    wsme['responses']['status'][to_swag_key(w)] = inspect_wm_schema(d, False)
+                    doc = inspect.getdoc(d)
+                    if doc != None:
+                        wsme['responses']['status']['description'] = doc
+            elif w == 'status_code':
+                wsme['responses'][d] = wsme['responses']['status']
+                del wsme['responses']['status']
+            #elif w == 'body_type':
+            #    wsme[to_swag_key(w)] = get_type_str(d)
+            #elif w == 'extra_options' or w == 'ignore_extra_args' \
+            #     or w == 'name' or w == 'pass_request':
+            #    wsme[to_swag_key(w)] = d
+            #else:
+            #    wsme[to_swag_key(w)] = d
+        return wsme
+
+    c = _hierarchy[name]
+    wsme_defs = {}
+    for k, v in inspect.getmembers(c):
+        if p_u.iscontroller(v):
+            for m, o in inspect.getmembers(v):
+                if m == "_wsme_definition":
+                    wsme_defs[k] = get_wm_def(o)
+
+    # output wsme info into files by each controller for dev
+    #import pprint
+    #with open(name + '.txt', 'w') as fout:
+    #    pprint.pprint(wsme_defs, stream=fout, indent=2)
+
+    return wsme_defs
 
 
 def get_controllers(name):
@@ -113,6 +337,7 @@ def get_controllers(name):
         controllers.update(cacts)
     return controllers
 
+
 def get_paths():
     """
     return all the registered paths
@@ -126,7 +351,8 @@ def get_paths():
     for name in _hierarchy:
         fullpath = build_path(get_swag(name))
         controllers = get_controllers(name)
-        paths = get_controller_paths(controllers)
+        wsme_defs = get_wsme_defs(name)
+        paths = get_controller_paths(controllers, wsme_defs)
         for path in paths:
             ptuple = (path_join(fullpath, path[0]), path[1])
             pathlist.append(ptuple)
@@ -172,7 +398,7 @@ def methods_get(name):
     if hasattr(c, 'index') and p_u.iscontroller(c.index):
         cfg = p_u._cfg(c.index)
         if cfg.get('generic_handlers').get('DEFAULT'):
-            mlist.append('GET')
+            mlist.append('get')
         mlist += cfg.get('allowed_methods')
     for i in c.__dict__:
         ii = getattr(c, i)
--- pecan_swagger/utils.py
+++ pecan_swagger/utils.py
@@ -17,10 +17,47 @@ def swagger_build(title, version):
     swag = dict()
     swag['swagger'] = '2.0'
     swag['info'] = dict(title=title, version=version)
+    swag['consumes'] = []
+    swag['produces'] = []
     swag['paths'] = {}
     for p in g.get_paths():
         if p[0] not in swag['paths']:
-            swag['paths'][p[0]] = {k: {} for k in p[1]}
+            swag['paths'][p[0]] = _tuple_to_dict(p[1])
         elif len(p[1]) > 0:
-            swag['paths'][p[0]].update({k: {} for k in p[1]})
+            swag['paths'][p[0]].update(_tuple_to_dict(p[1]))
     return swag
+
+
+def _tuple_to_dict(tpl):
+    """Convert tuple to dictionary
+    
+    each tuple must have key and value.
+    This function arrows taple including lists.
+
+    ex.) acceptable taple
+    OK: ('/', ('GET',(('desc',{}),('res',{}),('params',['id','name']))))
+    NG: ('/', ('GET',('desc','res',('params',['id','name']))))
+    """
+    if isinstance(tpl, list):
+        d = []
+        for e in tpl:
+            d.append(_tuple_to_dict(e))
+    elif isinstance(tpl, tuple):
+        d = {}
+        if isinstance(tpl[0], tuple):
+            # tuple has some child tuple
+            for e in tpl:
+                d[e[0]] = _tuple_to_dict(e[1])
+        elif isinstance(tpl[0], list):
+            # list member should be processed recursively
+            d = _tuple_to_dict(tpl[0])
+        else:
+            if len(tpl) == 2:
+                # single tuple node
+                d[tpl[0]] = _tuple_to_dict(tpl[1])
+            else:
+                raise Exception(tpl)
+    else:
+        # value or dict
+        d = tpl
+    return d
